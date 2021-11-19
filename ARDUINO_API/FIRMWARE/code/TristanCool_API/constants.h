bcce52f94106c9fab284edd82f82eab79c9a9ef0
tristan-f-cool <tristan.f.cool@gmail.com>
2021-11-18 21:11:53 -0500

    update common project header files & populate Arduino Release Notes

--- ARDUINO_API/FIRMWARE/code/TristanCool_API/constants.h
+++ ARDUINO_API/FIRMWARE/code/TristanCool_API/constants.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5125e213ab2930766c4a4b1ad2a485e63b68907a20a98960a3872b3255b2e650
-size 220
+oid sha256:b5fdf3027a3ad65f0b5b3628c6a3267bcdadc3b4a8b4ea2a352819c63a110f02
+size 225
