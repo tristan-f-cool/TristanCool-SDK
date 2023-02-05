7b393cef13d9682c2eefec08183d9a3dbcaaf552
tristan-f-cool <tristan.f.cool@gmail.com>
2023-02-05 15:58:28 -0500

    Shift Register PISO firmware code

---
+++ ARDUINO_API/FIRMWARE/projects/shift_register/shift_register_SIPO.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b5d1e1978b2d4bef54bf2e1da986422448faa295731c55e5d14988e3ac999fd
+size 1538
---
+++ ARDUINO_API/FIRMWARE/projects/shift_register_SIPO/shift_register_SIPO.ino
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1e0f3d484cf89aee8dd1f6880174a29f321cd1fec70e798b1decb92e6c5b042
+size 3519
